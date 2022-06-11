79ae5d04862568b30a07e0cccc5156d9bde7d362
amiti2 <arcirodha1@gmail.com>
2022-06-11 23:10:55 +0530

    adding ignore files

---
+++ Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+namespace Operators
+{
+    class Program
+    {
+        public static void Main()
+        {
+            WriteLine("I am practicing and would achieve success soon");            
+        }
+    }
+}
