4cf854699a7d65c7acfcc5d74cc2bd25d3571aac
amiti2 <arcirodha1@gmail.com>
2022-06-11 23:27:31 +0530

    tried to copy Linuxes rythmbox

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -5,7 +5,24 @@ namespace Operators
     {
         public static void Main()
         {
-            WriteLine("I am practicing and would achieve success soon");            
+            WriteLine("I am practicing and would achieve success soon");      
+            int a = 3;
+            int b = a++;
+            
+            //A should be 4
+            //B should be 3
+            
+            WriteLine($"value of A is {a} and b is {b}");
+
+            int c = 3;
+            int d = ++c;
+
+            //pre increment
+            //C should be 4
+            //D should be 4
+
+            WriteLine($"Value of C is {c} and D is {d}");            
+
         }
     }
 }
